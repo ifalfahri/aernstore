b85fb47b4444d39cd75ec49b1cb6be9a645d0adf
IfalEXZ <ifaltampan69@gmail.com>
2024-09-28 22:48:11 +0800

    add umami analytics to ur head

---
+++ app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+    return (
+      <>
+        <script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="95ff212a-1b5d-439f-b1ea-41d2af07bf2a"
+        ></script>
+      </>
+    );
+  }
+  
